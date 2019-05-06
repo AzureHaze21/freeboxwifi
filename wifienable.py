337135a42dc84cb3acaed5443dfe0bfce2ae3599
pyoung21 <30935238+pyoung21@users.noreply.github.com>
2019-05-06 14:06:46 +0200

    Update wifienable.py

--- wifienable.py
+++ wifienable.py
@@ -6,7 +6,7 @@ import browser_cookie3
 for cookie in browser_cookie3.load():
 	if 'mafreebox.freebox.fr' in cookie.domain:
 		print('Cookie: ' + cookie.name + '=' + cookie.value)
-		url = 'http://mafreebox.free.fr/api/v2/wifi/config/'
+		url = 'http://mafreebox.free.fr/api/v4/wifi/config/'
 		print('Shutting down wifi...') if sys.argv[1]=='disable' else print('Turning on wifi...')
 		res = requests.put(url, headers={ 'Cookie': cookie.name+'='+cookie.value }, json={ "enabled": False if sys.argv[1]=='disable' else True })
 		print(res.json())
